bfe32fcf7962756de0a7af792d4aa795029c7eae
davi-canuto <davicanutogregorio@gmail.com>
2025-01-27 19:30:28 -0300

    wip: start wortk in graphs

---
+++ Graph/GraphTest.java
@@ -0,0 +1,30 @@
+package Graph;
+
+public class Graph {
+  public Vertex[] endVertex(Edge e) {
+  }
+
+  public Vertex opposite(Vertex v, Edge e) {
+  }
+
+  public boolean isAdjacent(Vertex v, Vertex w) {
+  }
+
+  public void replace(Vertex v, Object x) {
+  }
+
+  public void replace(Edge e, Object x) {
+  }
+
+  public Vertex insertVertex(Object o) {
+  }
+
+  public Edge insertEdge(Vertex v, Vertex w, Object o) {
+  }
+
+  public Vertex removeVertex(Vertex v) {
+  }
+
+  public Edge removeEdge(Edge e) {
+  }
+}
